873ef5a184e051f50bffed58db74a9f00ccb6db0
joba288 <banksjoel1971@gmail.com>
2023-08-27 13:55:52 -0700

    person class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 #include "raylib.h"
+
+
+
+#include "./Person.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -9,21 +14,37 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1080;
+    const int screenHeight = 768;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Memory");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+
+
+    Texture2D hair = LoadTexture("resources/hair1.png");
+    Texture2D head = LoadTexture("resources/head1.png");
+    Texture2D eyes = LoadTexture("resources/eyes1.png");
+    Texture2D nose = LoadTexture("resources/nose1.png");
+    Texture2D mouth = LoadTexture("resources/mouth1.png");
+    Texture2D body = LoadTexture("resources/body1.png");
+    Texture2D hand = LoadTexture("resources/hand1.png");
+    Texture2D foot = LoadTexture("resources/foot1.png");
+
+    Person person{Vector2{300.f, 300.f}, head, hair, eyes, nose, mouth, body, hand, foot, SKYBLUE, BROWN, PINK};
+
+
+
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+
+
+
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -31,7 +52,8 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            person.Draw();
+            person.scale = sin(GetTime());
 
         EndDrawing();
         //----------------------------------------------------------------------------------
